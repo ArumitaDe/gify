4a08ff62c421ae4a49f79957f04032289e730fd0
Arumita De <Arumita De>
2017-03-08 22:33:07 -0500

    adding files

--- main.js
+++ main.js
@@ -55,28 +55,13 @@ $("#section1").click(function(event) {
         // After the data from the AJAX request comes back
         .done(function(response) {
 
-            // Saving the image_original_url property
-            /* var imageUrl = response.data.image_original_url;
-               console.log(imageUrl)
-                console.log(((imageUrl).slice(0,(imageUrl.length-4)))+"_s.gif")
-                var stillUrl=((imageUrl).slice(0,(imageUrl.length-4)))+"_s.gif";
-             // Creating and storing an image tag
-               var animalDiv = $("<div>");
-
-                 // Creating a paragraph tag with the result item's rating
-                 var p = $("<p>").text("Rating: " + response.data.rating);
-               console.log("rating is " + response.data.rating);
-             var Image = $("<img>");
-              */
+            
             console.log(response);
 
 
-            // Step 2: since the image information is inside of the data key,
-            // make a variable named results and set it equal to response.data
-
-            // =============== put step 2 in between these dashes ==================
+            
             var results = response.data;
-            // ========================
+            
 
             for (var i = 0; i < results.length; i++) {
                 var animalDiv = $("<div >");
@@ -93,34 +78,19 @@ $("#section1").click(function(event) {
                 animalDiv.append(Image);
                 $("#images").prepend(animalDiv);
 
-                // Step 3: uncomment the for loop above and the closing curly bracket below.
-                // Make a div with jQuery and store it in a variable named animalDiv.
-                // Make a paragraph tag with jQuery and store it in a variable named p.
-                // Set the inner text of the paragraph to the rating of the image in results[i].
-                // Make an image tag with jQuery and store it in a variable named animalImage.
-                // Set the image's src to results[i]'s fixed_height.url.
-                // Append the p variable to the animalDiv variable.
-                // Append the animalImage variable to the animalDiv variable.
-                // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
-
-                // ============= put step 3 in between these dashes ======================
-
-                // ==================================
+                
             }
 
 
-            // Setting the catImage src attribute to imageUrl
+           
 
-
-
-
-            // Prepending the catImage to the images div
+            // Prepending the Image to the images div
 
             // Appending the paragraph and image tag to the animalDiv
             animalDiv.append(p);
             animalDiv.append(Image);
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            // Prependng the animalDiv to the HTML page in the "#images" div
 
             $("#images").prepend(animalDiv);
         });
